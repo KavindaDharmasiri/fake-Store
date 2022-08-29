abddcf116b758348f4c4af01b8cddaf7e8552b81
KavindaDharmasiri <git config --global user.name FIRST_NAME>
2022-08-29 11:13:31 +0530

    update Drawer

--- src/app/App.js
+++ src/app/App.js
@@ -11,9 +11,9 @@ import {Routes, Route} from "react-router-dom";
 function App() {
   return (
       <Routes>
+          <Route index element={<Login />} />
+          <Route path='/register' element={<Register/>}/>
           <Route path="/" element={<MiniDrawer />}>
-              <Route index element={<Login />} />
-              <Route path='/register' element={<Register/>}/>
               <Route path='/dashBoard' element={<DashBoard/>}/>
               <Route path='/product' element={<Product/>}/>
               <Route path='/cart' element={<Cart/>}/>
